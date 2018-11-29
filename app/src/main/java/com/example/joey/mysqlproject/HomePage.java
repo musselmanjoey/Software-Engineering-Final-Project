4084295de98dde6d283608b9b57c1e5c59a46858
bennettbridges <bab14k@my.fsu.edu>
2018-11-29 16:24:49 -0500

    Added other scrolling activites, need to change the Set activity to a scrolling activity so all the information will fit

--- app/src/main/java/com/example/joey/mysqlproject/HomePage.java
+++ app/src/main/java/com/example/joey/mysqlproject/HomePage.java
@@ -88,11 +88,15 @@ public class HomePage extends AppCompatActivity
         {
             startActivity(new Intent(this,SetSchedule.class));
         }
-        else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        else if (id == R.id.nav_checkschedule)
+        {
+            startActivity(new Intent(this,CheckSchedule.class));
+        }
+        else if (id == R.id.nav_seeother)
+        {
+            startActivity(new Intent(this,SeeOtherSchedules.class));
+        }
+        else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
