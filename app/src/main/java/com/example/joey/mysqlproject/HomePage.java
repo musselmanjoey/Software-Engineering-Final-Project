dd74548898289e9d7a9739a8ed673caa80cec62e
bennettbridges <bab14k@my.fsu.edu>
2018-12-04 10:49:13 -0500

    Edited home page layout need to make font pop

--- app/src/main/java/com/example/joey/mysqlproject/HomePage.java
+++ app/src/main/java/com/example/joey/mysqlproject/HomePage.java
@@ -36,16 +36,6 @@ public class HomePage extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -139,15 +129,7 @@ public class HomePage extends AppCompatActivity
             myintent.putExtras(mybundle);
             startActivity(myintent);
         }
-        else if (id == R.id.nav_send)
-        {
-            Intent myintent = new Intent(this,Messages.class);
-            Bundle mybundle = new Bundle();
 
-            mybundle.putString("Busername",username);
-            myintent.putExtras(mybundle);
-            startActivity(myintent);
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
